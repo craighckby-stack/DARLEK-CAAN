c78ff536828b3a3134e83c45211c2c49538641bd
Craig Huckerby <craighckby@gmail.com>
2026-09-05 18:13:47 +1000

    EMG Core v49: Neural Optimization on .next_dev/server/middleware-build-manifest.js

--- .next_dev/server/middleware-build-manifest.js
+++ .next_dev/server/middleware-build-manifest.js
@@ -1,22 +1,39 @@
-self.__BUILD_MANIFEST = {
-  "polyfillFiles": [
-    "static/chunks/polyfills.js"
-  ],
-  "devFiles": [],
-  "ampDevFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/webpack.js",
-    "static/chunks/main-app.js"
-  ],
-  "rootMainFilesTree": {},
-  "pages": {
-    "/_app": []
-  },
-  "ampFirstPages": []
-};
-self.__BUILD_MANIFEST.lowPriorityFiles = [
-"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+(function() {
+  "use strict";
 
-];
+  var targetScope = typeof self !== "undefined" ? self : (typeof globalThis !== "undefined" ? globalThis : this);
+
+  var getSafeBuildId = function() {
+    if (typeof process !== "undefined" && process && process.env && typeof process.env.__NEXT_BUILD_ID === "string") {
+      var rawId = process.env.__NEXT_BUILD_ID;
+      if (/^[a-zA-Z0-9_\-]+$/.test(rawId)) {
+        return rawId;
+      }
+      return encodeURIComponent(rawId);
+    }
+    return "development";
+  };
+
+  var safeBuildId = getSafeBuildId();
+
+  targetScope.__BUILD_MANIFEST = {
+    "polyfillFiles": [
+      "static/chunks/polyfills.js"
+    ],
+    "devFiles": [],
+    "ampDevFiles": [],
+    "lowPriorityFiles": [
+      "/static/" + safeBuildId + "/_buildManifest.js",
+      "/static/" + safeBuildId + "/_ssgManifest.js"
+    ],
+    "rootMainFiles": [
+      "static/chunks/webpack.js",
+      "static/chunks/main-app.js"
+    ],
+    "rootMainFilesTree": {},
+    "pages": {
+      "/_app": []
+    },
+    "ampFirstPages": []
+  };
+})();
