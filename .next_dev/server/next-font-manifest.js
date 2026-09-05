25825ec8a8062dd15629ded5a378ab81ab904c44
Craig Huckerby <craighckby@gmail.com>
2026-09-05 18:14:21 +1000

    EMG Core v49: Neural Optimization on .next_dev/server/next-font-manifest.js

--- .next_dev/server/next-font-manifest.js
+++ .next_dev/server/next-font-manifest.js
@@ -1,1 +1,33 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
+(function () {
+  "use strict";
+  
+  var globalScope = typeof globalThis !== "undefined"
+    ? globalThis
+    : typeof self !== "undefined"
+      ? self
+      : typeof window !== "undefined"
+        ? window
+        : typeof global !== "undefined"
+          ? global
+          : {};
+
+  var manifestPayload = JSON.stringify(
+    Object.freeze({
+      pages: Object.freeze({}),
+      app: Object.freeze({}),
+      appUsingSizeAdjust: false,
+      pagesUsingSizeAdjust: false
+    })
+  );
+
+  try {
+    Object.defineProperty(globalScope, "__NEXT_FONT_MANIFEST", {
+      value: manifestPayload,
+      writable: true,
+      enumerable: true,
+      configurable: true
+    });
+  } catch (e) {
+    globalScope.__NEXT_FONT_MANIFEST = manifestPayload;
+  }
+})();
