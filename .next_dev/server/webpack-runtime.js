e036d9c4cf607a085f00919e207a13f3f0c7fa19
Craig Huckerby <craighckby@gmail.com>
2026-09-05 21:44:49 +1000

    EMG Core v49: Neural Optimization on .next_dev/server/webpack-runtime.js

--- .next_dev/server/webpack-runtime.js
+++ .next_dev/server/webpack-runtime.js
@@ -1,45 +1,34 @@
-/*
- * ATTENTION: An "eval-source-map" devtool has been used.
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
+/******/ 	// The module cache with optimized null-prototype mapping
+/******/ 	var __webpack_module_cache__ = Object.create(null);
 /******/ 	
-/******/ 	// The require function
+/******/ 	// The optimized require function with unrolled cache lookup and reduced allocations
 /******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
 /******/ 		var cachedModule = __webpack_module_cache__[moduleId];
 /******/ 		if (cachedModule !== undefined) {
 /******/ 			return cachedModule.exports;
 /******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
+/******/ 		
 /******/ 		var module = __webpack_module_cache__[moduleId] = {
 /******/ 			id: moduleId,
 /******/ 			loaded: false,
 /******/ 			exports: {}
 /******/ 		};
 /******/ 	
-/******/ 		// Execute the module function
 /******/ 		var threw = true;
 /******/ 		try {
 /******/ 			__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
 /******/ 			threw = false;
 /******/ 		} finally {
-/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 			if (threw) {
+/******/ 				__webpack_module_cache__[moduleId] = undefined;
+/******/ 			}
 /******/ 		}
 /******/ 	
-/******/ 		// Flag the module as loaded
 /******/ 		module.loaded = true;
-/******/ 	
-/******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
 /******/ 	
@@ -54,11 +43,10 @@
 /******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
 /******/ 		__webpack_require__.n = (module) => {
 /******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
+/******/ 				() => module.default :
+/******/ 				() => module;
 /******/ 			__webpack_require__.d(getter, { a: getter });
 /******/ 			return getter;
 /******/ 		};
@@ -66,29 +54,27 @@
 /******/ 	
 /******/ 	/* webpack/runtime/create fake namespace object */
 /******/ 	(() => {
-/******/ 		var getProto = Object.getPrototypeOf ? (obj) => (Object.getPrototypeOf(obj)) : (obj) => (obj.__proto__);
+/******/ 		var getProto = Object.getPrototypeOf ? (obj) => Object.getPrototypeOf(obj) : (obj) => obj.__proto__;
 /******/ 		var leafPrototypes;
-/******/ 		// create a fake namespace object
-/******/ 		// mode & 1: value is a module id, require it
-/******/ 		// mode & 2: merge all properties of value into the ns
-/******/ 		// mode & 4: return value when already ns object
-/******/ 		// mode & 16: return value when it's Promise-like
-/******/ 		// mode & 8|1: behave like require
 /******/ 		__webpack_require__.t = function(value, mode) {
-/******/ 			if(mode & 1) value = this(value);
-/******/ 			if(mode & 8) return value;
-/******/ 			if(typeof value === 'object' && value) {
-/******/ 				if((mode & 4) && value.__esModule) return value;
-/******/ 				if((mode & 16) && typeof value.then === 'function') return value;
+/******/ 			if (mode & 1) value = this(value);
+/******/ 			if (mode & 8) return value;
+/******/ 			if (typeof value === 'object' && value) {
+/******/ 				if ((mode & 4) && value.__esModule) return value;
+/******/ 				if ((mode & 16) && typeof value.then === 'function') return value;
 /******/ 			}
 /******/ 			var ns = Object.create(null);
 /******/ 			__webpack_require__.r(ns);
 /******/ 			var def = {};
 /******/ 			leafPrototypes = leafPrototypes || [null, getProto({}), getProto([]), getProto(getProto)];
-/******/ 			for(var current = mode & 2 && value; typeof current == 'object' && !~leafPrototypes.indexOf(current); current = getProto(current)) {
-/******/ 				Object.getOwnPropertyNames(current).forEach((key) => (def[key] = () => (value[key])));
+/******/ 			for (var current = mode & 2 && value; typeof current === 'object' && !~leafPrototypes.indexOf(current); current = getProto(current)) {
+/******/ 				var names = Object.getOwnPropertyNames(current);
+/******/ 				for (var i = 0, len = names.length; i < len; i++) {
+/******/ 					var key = names[i];
+/******/ 					def[key] = (k => () => value[k])(key);
+/******/ 				}
 /******/ 			}
-/******/ 			def['default'] = () => (value);
+/******/ 			def.default = () => value;
 /******/ 			__webpack_require__.d(ns, def);
 /******/ 			return ns;
 /******/ 		};
@@ -96,10 +82,9 @@
 /******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
 /******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 			for (var key in definition) {
+/******/ 				if (__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
 /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
 /******/ 				}
 /******/ 			}
@@ -109,40 +94,37 @@
 /******/ 	/* webpack/runtime/ensure chunk */
 /******/ 	(() => {
 /******/ 		__webpack_require__.f = {};
-/******/ 		// This file contains only the entry chunk.
-/******/ 		// The chunk loading function for additional chunks
 /******/ 		__webpack_require__.e = (chunkId) => {
-/******/ 			return Promise.all(Object.keys(__webpack_require__.f).reduce((promises, key) => {
-/******/ 				__webpack_require__.f[key](chunkId, promises);
-/******/ 				return promises;
-/******/ 			}, []));
+/******/ 			var f = __webpack_require__.f;
+/******/ 			var keys = Object.keys(f);
+/******/ 			var promises = [];
+/******/ 			for (var i = 0, len = keys.length; i < len; i++) {
+/******/ 				f[keys[i]](chunkId, promises);
+/******/ 			}
+/******/ 			return Promise.all(promises);
 /******/ 		};
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/get javascript chunk filename */
 /******/ 	(() => {
-/******/ 		// This function allow to reference async chunks and sibling chunks for the entrypoint
-/******/ 		__webpack_require__.u = (chunkId) => {
-/******/ 			// return url for filenames based on template
-/******/ 			return "" + chunkId + ".js";
-/******/ 		};
+/******/ 		__webpack_require__.u = (chunkId) => "" + chunkId + ".js";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("01e0960730372e58")
+/******/ 		__webpack_require__.h = () => "01e0960730372e58";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 		var hasOwnProperty = Object.prototype.hasOwnProperty;
+/******/ 		__webpack_require__.o = (obj, prop) => hasOwnProperty.call(obj, prop);
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
 /******/ 	(() => {
-/******/ 		// define __esModule on exports
 /******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			if (typeof Symbol !== 'undefined' && Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
@@ -161,60 +143,49 @@
 /******/ 	/* webpack/runtime/startup entrypoint */
 /******/ 	(() => {
 /******/ 		__webpack_require__.X = (result, chunkIds, fn) => {
-/******/ 			// arguments: chunkIds, moduleId are deprecated
 /******/ 			var moduleId = chunkIds;
-/******/ 			if(!fn) chunkIds = result, fn = () => (__webpack_require__(__webpack_require__.s = moduleId));
-/******/ 			chunkIds.map(__webpack_require__.e, __webpack_require__)
+/******/ 			if (!fn) chunkIds = result, fn = () => __webpack_require__(__webpack_require__.s = moduleId);
+/******/ 			for (var i = 0, len = chunkIds.length; i < len; i++) {
+/******/ 				__webpack_require__.e(chunkIds[i]);
+/******/ 			}
 /******/ 			var r = fn();
 /******/ 			return r === undefined ? result : r;
-/******/ 		}
+/******/ 		};
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/require chunk loading */
 /******/ 	(() => {
-/******/ 		// no baseURI
-/******/ 		
-/******/ 		// object to store loaded chunks
-/******/ 		// "1" means "loaded", otherwise not loaded yet
 /******/ 		var installedChunks = {
 /******/ 			"webpack-runtime": 1
 /******/ 		};
 /******/ 		
-/******/ 		// no on chunks loaded
-/******/ 		
 /******/ 		var installChunk = (chunk) => {
 /******/ 			var moreModules = chunk.modules, chunkIds = chunk.ids, runtime = chunk.runtime;
-/******/ 			for(var moduleId in moreModules) {
-/******/ 				if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 			for (var moduleId in moreModules) {
+/******/ 				if (__webpack_require__.o(moreModules, moduleId)) {
 /******/ 					__webpack_require__.m[moduleId] = moreModules[moduleId];
 /******/ 				}
 /******/ 			}
-/******/ 			if(runtime) runtime(__webpack_require__);
-/******/ 			for(var i = 0; i < chunkIds.length; i++)
+/******/ 			if (runtime) runtime(__webpack_require__);
+/******/ 			for (var i = 0, len = chunkIds.length; i < len; i++) {
 /******/ 				installedChunks[chunkIds[i]] = 1;
-/******/ 		
+/******/ 			}
 /******/ 		};
 /******/ 		
-/******/ 		// require() chunk loading for javascript
 /******/ 		__webpack_require__.f.require = (chunkId, promises) => {
-/******/ 			// "1" is the signal for "already loaded"
-/******/ 			if(!installedChunks[chunkId]) {
-/******/ 				if("webpack-runtime" != chunkId) {
+/******/ 			if (!installedChunks[chunkId]) {
+/******/ 				if ("webpack-runtime" !== chunkId) {
 /******/ 					installChunk(require("./" + __webpack_require__.u(chunkId)));
-/******/ 				} else installedChunks[chunkId] = 1;
+/******/ 				} else {
+/******/ 					installedChunks[chunkId] = 1;
+/******/ 				}
 /******/ 			}
 /******/ 		};
 /******/ 		
 /******/ 		module.exports = __webpack_require__;
 /******/ 		__webpack_require__.C = installChunk;
-/******/ 		
-/******/ 		// no HMR
-/******/ 		
-/******/ 		// no HMR manifest
 /******/ 	})();
 /******/ 	
 /************************************************************************/
-/******/ 	
-/******/ 	
 /******/ })()
 ;
