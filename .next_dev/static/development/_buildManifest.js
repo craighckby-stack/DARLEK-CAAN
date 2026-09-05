28db8bea27b269591fdc4d3d1af82449da9d2fdb
Craig Huckerby <craighckby@gmail.com>
2026-09-05 18:26:34 +1000

    EMG Core v49: Neural Optimization on .next_dev/static/development/_buildManifest.js

--- .next_dev/static/development/_buildManifest.js
+++ .next_dev/static/development/_buildManifest.js
@@ -1,1 +1,59 @@
-self.__BUILD_MANIFEST = (function(a){return {__rewrites:{afterFiles:[{has:a,source:"\u002F:path((?!api|_next|static|favicon.ico).*)",destination:"\u002F"}],beforeFiles:[],fallback:[]},__routerFilterStatic:a,__routerFilterDynamic:a,sortedPages:["\u002F_app"]}}(void 0));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+(function (globalContext) {
+  "use strict";
+
+  if (!globalContext || (typeof globalContext !== "object" && typeof globalContext !== "function")) {
+    return;
+  }
+
+  var deepFreeze = function (obj) {
+    if (obj !== null && (typeof obj === "object" || typeof obj === "function")) {
+      Object.freeze(obj);
+      var propNames = Object.getOwnPropertyNames(obj);
+      for (var i = 0; i < propNames.length; i++) {
+        var prop = obj[propNames[i]];
+        if (prop !== null && (typeof prop === "object" || typeof prop === "function") && !Object.isFrozen(prop)) {
+          deepFreeze(prop);
+        }
+      }
+    }
+    return obj;
+  };
+
+  var buildManifest = deepFreeze({
+    __rewrites: {
+      afterFiles: [
+        {
+          has: void 0,
+          source: "/:path((?!api|_next|static|favicon.ico).*)",
+          destination: "/"
+        }
+      ],
+      beforeFiles: [],
+      fallback: []
+    },
+    __routerFilterStatic: void 0,
+    __routerFilterDynamic: void 0,
+    sortedPages: ["/_app"]
+  });
+
+  try {
+    Object.defineProperty(globalContext, "__BUILD_MANIFEST", {
+      value: buildManifest,
+      writable: true,
+      enumerable: true,
+      configurable: true
+    });
+  } catch (_e) {
+    globalContext.__BUILD_MANIFEST = buildManifest;
+  }
+
+  if (typeof globalContext.__BUILD_MANIFEST_CB === "function") {
+    try {
+      globalContext.__BUILD_MANIFEST_CB();
+    } catch (cbError) {
+      if (typeof console !== "undefined" && typeof console.error === "function") {
+        console.error("Error executing __BUILD_MANIFEST_CB callback:", cbError);
+      }
+    }
+  }
+})(typeof globalThis !== "undefined" ? globalThis : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : this);
