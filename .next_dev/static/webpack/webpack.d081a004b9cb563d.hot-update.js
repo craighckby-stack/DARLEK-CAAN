26c3c5dba10e6353badeaa04c583d6dbe2b820e7
Craig Huckerby <craighckby@gmail.com>
2026-09-05 22:42:19 +1000

    EMG Core v49: Neural Optimization on .next_dev/static/webpack/webpack.d081a004b9cb563d.hot-update.js

--- .next_dev/static/webpack/webpack.d081a004b9cb563d.hot-update.js
+++ .next_dev/static/webpack/webpack.d081a004b9cb563d.hot-update.js
@@ -1,37 +1,45 @@
 "use strict";
+
 /**
  * Hardened Webpack HMR Runtime Module
  * Enforces defensive runtime bounds, context verification, and property tamper-proofing.
  */
-(function(globalContext) {
-  const targetGlobal = typeof globalThis !== "undefined" 
-    ? globalThis 
-    : typeof self !== "undefined" 
-      ? self 
-      : globalContext;
+(function initializeWebpackHmrRuntime(fallbackGlobalContext) {
+  const resolveTargetGlobal = () => {
+    if (typeof globalThis !== "undefined") return globalThis;
+    if (typeof self !== "undefined") return self;
+    return fallbackGlobalContext;
+  };
 
-  if (targetGlobal && typeof targetGlobal["webpackHotUpdate_N_E"] === "function") {
-    targetGlobal["webpackHotUpdate_N_E"](
-      "webpack",
-      Object.freeze(Object.create(null)),
-      function(__webpack_require__) {
-        if (!__webpack_require__ || (typeof __webpack_require__ !== "function" && typeof __webpack_require__ !== "object")) {
-          return;
-        }
-        
-        const HASH_VALUE = "d235413fa0cd6b0a";
-        
-        try {
-          Object.defineProperty(__webpack_require__, "h", {
-            value: () => HASH_VALUE,
-            writable: false,
-            enumerable: true,
-            configurable: false
-          });
-        } catch {
-          __webpack_require__.h = () => HASH_VALUE;
-        }
-      }
-    );
+  const targetGlobal = resolveTargetGlobal();
+  const hmrUpdateFunction = targetGlobal?.["webpackHotUpdate_N_E"];
+
+  if (typeof hmrUpdateFunction !== "function") {
+    return;
   }
+
+  const MODULE_NAME = "webpack";
+  const EMPTY_FROZEN_MODULES = Object.freeze(Object.create(null));
+  const TARGET_HASH_VALUE = "d235413fa0cd6b0a";
+
+  const injectModuleHashGetter = (webpackRequire) => {
+    if (!webpackRequire || (typeof webpackRequire !== "function" && typeof webpackRequire !== "object")) {
+      return;
+    }
+
+    const hashDescriptor = {
+      value: () => TARGET_HASH_VALUE,
+      writable: false,
+      enumerable: true,
+      configurable: false,
+    };
+
+    try {
+      Object.defineProperty(webpackRequire, "h", hashDescriptor);
+    } catch {
+      webpackRequire.h = hashDescriptor.value;
+    }
+  };
+
+  hmrUpdateFunction(MODULE_NAME, EMPTY_FROZEN_MODULES, injectModuleHashGetter);
 })(typeof window !== "undefined" ? window : globalThis);
