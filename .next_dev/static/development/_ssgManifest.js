3af1ded6d85d6c68a5105e396832d1f5f1c72203
Craig Huckerby <craighckby@gmail.com>
2026-09-05 22:01:56 +1000

    EMG Core v49: Neural Optimization on .next_dev/static/development/_ssgManifest.js

--- .next_dev/static/development/_ssgManifest.js
+++ .next_dev/static/development/_ssgManifest.js
@@ -5,11 +5,11 @@
  * @returns {typeof globalThis | Window | NodeJS.Global | unknown} The resolved global object.
  */
 function resolveGlobalScope() {
-  if (typeof globalThis !== 'undefined') return globalThis;
-  if (typeof self !== 'undefined') return self;
-  if (typeof window !== 'undefined') return window;
-  if (typeof global !== 'undefined') return global;
-  return this;
+  return typeof globalThis !== 'undefined' ? globalThis
+    : typeof self !== 'undefined' ? self
+    : typeof window !== 'undefined' ? window
+    : typeof global !== 'undefined' ? global
+    : this;
 }
 
 /**
@@ -18,26 +18,21 @@ function resolveGlobalScope() {
 (function initializeSsgManifest() {
   const globalScope = resolveGlobalScope();
 
-  const isInvalidScope = !globalScope || (typeof globalScope !== 'object' && typeof globalScope !== 'function');
-  if (isInvalidScope) {
+  if (!globalScope || (typeof globalScope !== 'object' && typeof globalScope !== 'function')) {
     return;
   }
 
   try {
-    const ManifestContainer = typeof Set === 'function' ? Set : Array;
-    
     Object.defineProperty(globalScope, '__SSG_MANIFEST', {
-      value: new ManifestContainer(),
+      value: new (typeof Set === 'function' ? Set : Array)(),
       writable: true,
       enumerable: true,
       configurable: true
     });
 
-    const initializationCallback = globalScope.__SSG_MANIFEST_CB;
-    if (typeof initializationCallback === 'function') {
-      Reflect.apply(initializationCallback, globalScope, []);
+    const cb = globalScope.__SSG_MANIFEST_CB;
+    if (typeof cb === 'function') {
+      cb.call(globalScope);
     }
-  } catch {
-    // Gracefully contain volatile execution and prevent unhandled runtime errors
-  }
+  } catch {}
 })();
