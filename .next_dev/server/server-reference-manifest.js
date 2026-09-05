5c66a4690dbcf56858a956fcc178ef71186f6dcb
Craig Huckerby <craighckby@gmail.com>
2026-09-05 18:15:36 +1000

    EMG Core v49: Neural Optimization on .next_dev/server/server-reference-manifest.js

--- .next_dev/server/server-reference-manifest.js
+++ .next_dev/server/server-reference-manifest.js
@@ -1,1 +1,11 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+'use strict';
+
+(function () {
+  const globalContext = typeof self !== 'undefined'
+    ? self
+    : (typeof globalThis !== 'undefined' ? globalThis : this);
+
+  if (globalContext) {
+    globalContext.__RSC_SERVER_MANIFEST = "{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}";
+  }
+})();
