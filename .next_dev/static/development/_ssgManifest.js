5e4e02610ed498a83b85d40a36bb525003bf2942
Craig Huckerby <craighckby@gmail.com>
2026-09-05 18:26:44 +1000

    EMG Core v49: Neural Optimization on .next_dev/static/development/_ssgManifest.js

--- .next_dev/static/development/_ssgManifest.js
+++ .next_dev/static/development/_ssgManifest.js
@@ -1,1 +1,34 @@
-self.__SSG_MANIFEST=new Set;self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
+'use strict';
+(function () {
+  const globalScope =
+    typeof globalThis !== 'undefined'
+      ? globalThis
+      : typeof self !== 'undefined'
+        ? self
+        : typeof window !== 'undefined'
+          ? window
+          : typeof global !== 'undefined'
+            ? global
+            : this;
+
+  if (!globalScope || (typeof globalScope !== 'object' && typeof globalScope !== 'function')) {
+    return;
+  }
+
+  try {
+    const SafeSet = typeof Set === 'function' ? Set : Array;
+    Object.defineProperty(globalScope, '__SSG_MANIFEST', {
+      value: new SafeSet(),
+      writable: true,
+      enumerable: true,
+      configurable: true
+    });
+
+    const callback = globalScope.__SSG_MANIFEST_CB;
+    if (typeof callback === 'function') {
+      Reflect.apply(callback, globalScope, []);
+    }
+  } catch {
+    // Gracefully contain volatile execution and prevent unhandled runtime errors
+  }
+})();
