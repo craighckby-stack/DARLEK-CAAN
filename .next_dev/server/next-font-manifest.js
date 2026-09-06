fcc29649ee330ef790009cb60013e06ce3e996be
Craig Huckerby <craighckby@gmail.com>
2026-09-06 15:02:09 +1000

    EMG Core v49: Neural Optimization on .next_dev/server/next-font-manifest.js

--- .next_dev/server/next-font-manifest.js
+++ .next_dev/server/next-font-manifest.js
@@ -1,15 +1,16 @@
 (function initializeNextFontManifest() {
   "use strict";
 
-  const getGlobalScope = () => {
-    if (typeof globalThis !== "undefined") return globalThis;
-    if (typeof self !== "undefined") return self;
-    if (typeof window !== "undefined") return window;
-    if (typeof global !== "undefined") return global;
-    return {};
-  };
+  const globalScope = typeof globalThis !== "undefined" 
+    ? globalThis 
+    : typeof self !== "undefined" 
+      ? self 
+      : typeof window !== "undefined" 
+        ? window 
+        : typeof global !== "undefined" 
+          ? global 
+          : {};
 
-  const globalScope = getGlobalScope();
   const serializedManifest = '{"pages":{},"app":{},"appUsingSizeAdjust":false,"pagesUsingSizeAdjust":false}';
   const manifestSymbol = "__NEXT_FONT_MANIFEST";
 
