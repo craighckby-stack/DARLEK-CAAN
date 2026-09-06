cb63abdac3546854266edbb107529fcff5cd8922
Craig Huckerby <craighckby@gmail.com>
2026-09-06 16:38:18 +1000

    EMG Core v49: Neural Optimization on src/components/NeuralSimulator.tsx

--- src/components/NeuralSimulator.tsx
+++ src/components/NeuralSimulator.tsx
@@ -1,5 +1,11 @@
 'use client';
 
+/**
+ * @file NeuralSimulator.tsx
+ * @description Core export module for the Neural Simulator and AGI Cognitive Dashboard components.
+ * @version 4.9.0-EMG
+ */
+
 export {
   NeuralSimulator as default,
   NeuralSimulator,
