f8e803b56aea0c9294dc1a60dbdce52352a6ac9c
Craig Huckerby <craighckby@gmail.com>
2026-09-06 15:46:10 +1000

    EMG Core v49: Neural Optimization on src/components/ui/resizable.tsx

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -20,6 +20,24 @@ export type ResizableHandleProps = React.ComponentProps<
   readonly withHandle?: boolean
 }
 
+const RESIZABLE_PANEL_GROUP_CLASSES = "flex h-full w-full data-[panel-group-direction=vertical]:flex-col"
+
+const RESIZABLE_HANDLE_CLASSES = cn(
+  "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center",
+  "after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2",
+  "focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden",
+  "data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full",
+  "data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1",
+  "data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0",
+  "data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90"
+)
+
+const HANDLE_INNER_ELEMENT = (
+  <div className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs border">
+    <GripVerticalIcon className="size-2.5" />
+  </div>
+)
+
 /**
  * Container component for organizing resizable panels into rows or columns.
  */
@@ -30,10 +48,7 @@ const ResizablePanelGroup = React.memo(function ResizablePanelGroup({
   return (
     <ResizablePrimitive.PanelGroup
       data-slot="resizable-panel-group"
-      className={cn(
-        "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
-        className
-      )}
+      className={className ? `${RESIZABLE_PANEL_GROUP_CLASSES} ${className}` : RESIZABLE_PANEL_GROUP_CLASSES}
       {...props}
     />
   )
@@ -60,20 +75,17 @@ const ResizableHandle = React.memo(function ResizableHandle({
   className,
   ...props
 }: ResizableHandleProps) {
+  const computedClassName = React.useMemo(() => {
+    return className ? cn(RESIZABLE_HANDLE_CLASSES, className) : RESIZABLE_HANDLE_CLASSES
+  }, [className])
+
   return (
     <ResizablePrimitive.PanelResizeHandle
       data-slot="resizable-handle"
-      className={cn(
-        "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90",
-        className
-      )}
+      className={computedClassName}
       {...props}
     >
-      {withHandle && (
-        <div className="bg-border z-10 flex h-4 w-3 items-center justify-center rounded-xs border">
-          <GripVerticalIcon className="size-2.5" />
-        </div>
-      )}
+      {withHandle && HANDLE_INNER_ELEMENT}
     </ResizablePrimitive.PanelResizeHandle>
   )
 })
