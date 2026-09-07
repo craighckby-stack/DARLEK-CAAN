98500cbcc0d6d72c66a9ee50b3694f7a8ba2d4f6
Craig Huckerby <craighckby@gmail.com>
2026-09-07 15:22:09 +1000

    EMG Core v49: Neural Optimization on .next_dev/server/vendor-chunks/lucide-react.js

--- .next_dev/server/vendor-chunks/lucide-react.js
+++ .next_dev/server/vendor-chunks/lucide-react.js
@@ -35,11 +35,11 @@ const Icon = (0, react__WEBPACK_IMPORTED_MODULE_0__.forwardRef)(({
 
     const resolvedClassName = (0, _shared_src_utils_js__WEBPACK_IMPORTED_MODULE_2__.mergeClasses)("lucide", className);
 
-    const iconElements = iconNode.map(([tag, attrs], index) => 
+    const iconElements = iconNode && Array.isArray(iconNode) ? iconNode.map(([tag, attrs], index) => 
         (0, react__WEBPACK_IMPORTED_MODULE_0__.createElement)(tag, { key: attrs.key || index, ...attrs })
-    );
+    ) : [];
 
-    const resolvedChildren = Array.isArray(children) ? children : [children];
+    const resolvedChildren = Array.isArray(children) ? children : [children].filter(Boolean);
 
     return (0, react__WEBPACK_IMPORTED_MODULE_0__.createElement)(
         "svg",
@@ -110,7 +110,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (/* binding */ defaultAttributes)
 /* harmony export */ });
 
-const defaultAttributes = {
+const defaultAttributes = Object.freeze({
     xmlns: "http://www.w3.org/2000/svg",
     width: 24,
     height: 24,
@@ -120,7 +120,7 @@ const defaultAttributes = {
     strokeWidth: 2,
     strokeLinecap: "round",
     strokeLinejoin: "round"
-};
+});
 
 /* sourceMappingURL=defaultAttributes.js.map */
 `);
@@ -141,22 +141,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _createLucideIcon_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../createLucideIcon.js */ "(ssr)/./node_modules/lucide-react/dist/esm/createLucideIcon.js");
 
-const __iconNode = [
-    [
+const __iconNode = Object.freeze([
+    Object.freeze([
         "path",
-        {
+        Object.freeze({
             d: "M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",
             key: "5wwlr5"
-        }
-    ],
-    [
+        })
+    ]),
+    Object.freeze([
         "path",
-        {
+        Object.freeze({
             d: "M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",
             key: "1d0kgt"
-        }
-    ]
-];
+        })
+    ])
+]);
 
 const House = (0, _createLucideIcon_js__WEBPACK_IMPORTED_MODULE_0__["default"])("House", __iconNode);
 
@@ -179,36 +179,36 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _createLucideIcon_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../createLucideIcon.js */ "(ssr)/./node_modules/lucide-react/dist/esm/createLucideIcon.js");
 
-const __iconNode = [
-    [
+const __iconNode = Object.freeze([
+    Object.freeze([
         "path",
-        {
+        Object.freeze({
             d: "M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8",
             key: "v9h5vc"
-        }
-    ],
-    [
+        })
+    ]),
+    Object.freeze([
         "path",
-        {
+        Object.freeze({
             d: "M21 3v5h-5",
             key: "1q7to0"
-        }
-    ],
-    [
+        })
+    ]),
+    Object.freeze([
         "path",
-        {
+        Object.freeze({
             d: "M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16",
             key: "3uifl3"
-        }
-    ],
-    [
+        })
+    ]),
+    Object.freeze([
         "path",
-        {
+        Object.freeze({
             d: "M8 16H3v5",
             key: "1cv678"
-        }
-    ]
-];
+        })
+    ])
+]);
 
 const RefreshCw = (0, _createLucideIcon_js__WEBPACK_IMPORTED_MODULE_0__["default"])("RefreshCw", __iconNode);
 
@@ -231,29 +231,29 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _createLucideIcon_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../createLucideIcon.js */ "(ssr)/./node_modules/lucide-react/dist/esm/createLucideIcon.js");
 
-const __iconNode = [
-    [
+const __iconNode = Object.freeze([
+    Object.freeze([
         "path",
-        {
+        Object.freeze({
             d: "m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3",
             key: "wmoenq"
-        }
-    ],
-    [
+        })
+    ]),
+    Object.freeze([
         "path",
-        {
+        Object.freeze({
             d: "M12 9v4",
             key: "juzpu7"
-        }
-    ],
-    [
+        })
+    ]),
+    Object.freeze([
         "path",
-        {
+        Object.freeze({
             d: "M12 17h.01",
             key: "p32p05"
-        }
-    ]
-];
+        })
+    ])
+]);
 
 const TriangleAlert = (0, _createLucideIcon_js__WEBPACK_IMPORTED_MODULE_0__["default"])("TriangleAlert", __iconNode);
 
@@ -276,22 +276,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _createLucideIcon_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../createLucideIcon.js */ "(ssr)/./node_modules/lucide-react/dist/esm/createLucideIcon.js");
 
-const __iconNode = [
-    [
+const __iconNode = Object.freeze([
+    Object.freeze([
         "path",
-        {
+        Object.freeze({
             d: "M18 6 6 18",
             key: "1bl5f8"
-        }
-    ],
-    [
+        })
+    ]),
+    Object.freeze([
         "path",
-        {
+        Object.freeze({
             d: "m6 6 12 12",
             key: "d8bk6v"
-        }
-    ]
-];
+        })
+    ])
+]);
 
 const X = (0, _createLucideIcon_js__WEBPACK_IMPORTED_MODULE_0__["default"])("X", __iconNode);
 
@@ -313,10 +313,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   toKebabCase: () => (/* binding */ toKebabCase)
 /* harmony export */ });
 
-const toKebabCase = (string) => string.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase();
+const toKebabCase = (string) => typeof string === "string" ? string.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase() : "";
 
 const mergeClasses = (...classes) => classes
-    .filter((className, index, array) => Boolean(className) && className.trim() !== "" && array.indexOf(className) === index)
+    .flat(Infinity)
+    .filter((className, index, array) => typeof className === "string" && className.trim() !== "" && array.indexOf(className) === index)
     .join(" ")
     .trim();
 
