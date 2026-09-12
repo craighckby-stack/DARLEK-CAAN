c6ad34a3adb5a17924b78389b8548bc1797c3a59
Craig Huckerby <craighckby@gmail.com>
2026-09-12 17:59:13 +1000

    EMG Core: Refactoring on src/components/ui/separator.tsx

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -5,7 +5,7 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
 import { cn } from "@/lib/utils"
 
-export type SeparatorProps = React.ComponentProps<typeof SeparatorPrimitive.Root>
+export type SeparatorProps = React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
 
 const SEPARATOR_BASE_STYLES = "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px" as const
 
