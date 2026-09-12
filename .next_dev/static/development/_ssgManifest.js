69f08b1168476a1c716adad7839750ee3a7ce006
Craig Huckerby <craighckby@gmail.com>
2026-09-12 18:54:41 +1000

    [DARLEK CANN] Mutate .next_dev/static/development/_ssgManifest.js

--- .next_dev/static/development/_ssgManifest.js
+++ .next_dev/static/development/_ssgManifest.js
@@ -1,1 +1,8 @@
+/**
+ * DARLEK CANN ARCHITECTURAL HEADER
+ * File: .next_dev/static/development/_ssgManifest.js
+ * Role: Core system component participating in autonomous cognitive evolution cycles.
+ * Architecture: Type-safe modular unit with resilient state interfaces.
+ */
+
 self.__SSG_MANIFEST=new Set;self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
