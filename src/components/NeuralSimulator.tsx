6225741edb7a586f9c2fe6746d022f10c6b0b7ef
Craig Huckerby <craighckby@gmail.com>
2026-09-12 19:14:25 +1000

    [DARLEK CANN] Mutate src/components/NeuralSimulator.tsx

--- src/components/NeuralSimulator.tsx
+++ src/components/NeuralSimulator.tsx
@@ -1,3 +1,10 @@
+/**
+ * DARLEK CANN ARCHITECTURAL HEADER
+ * File: src/components/NeuralSimulator.tsx
+ * Role: Core system component participating in autonomous cognitive evolution cycles.
+ * Architecture: Type-safe modular unit with resilient state interfaces.
+ */
+
 'use strict';
 
 /**
