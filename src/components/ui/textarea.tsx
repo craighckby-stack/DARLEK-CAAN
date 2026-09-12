70c0f3acf63f6f9a6d4c6eb9a2804949cb294485
Craig Huckerby <craighckby@gmail.com>
2026-09-12 13:29:47 +1000

    EMG Core: Refactoring on src/components/ui/textarea.tsx

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,7 +1,10 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-export interface TextareaProps extends React.ComponentProps<"textarea"> {}
+export interface TextareaProps extends React.ComponentProps<"textarea"> {
+  maxLength?: number
+  onContentLengthExceeded?: (length: number) => void
+}
 
 const TEXTAREA_STYLES = [
   "flex field-sizing-content min-h-16 w-full rounded-md border",
@@ -15,10 +18,21 @@ const TEXTAREA_STYLES = [
 
 const Textarea = React.memo(
   React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-    function Textarea({ className, ...props }, ref) {
+    function Textarea({ className, maxLength, onContentLengthExceeded, onChange, ...props }, ref) {
       const mergedClassName = React.useMemo(
         () => cn(TEXTAREA_STYLES, className),
         [className]
+      )
+
+      const handleChange = React.useCallback(
+        (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+          const value = e.target.value
+          if (maxLength !== undefined && value.length > maxLength) {
+            onContentLengthExceeded?.(value.length)
+          }
+          onChange?.(e)
+        },
+        [maxLength, onContentLengthExceeded, onChange]
       )
 
       return (
@@ -26,8 +40,10 @@ const Textarea = React.memo(
           ref={ref}
           dir="ltr"
           data-slot="textarea"
+          maxLength={maxLength}
           className={mergedClassName}
           style={{ unicodeBidi: 'normal', direction: 'ltr', ...(props.style ?? {}) }}
+          onChange={handleChange}
           {...props}
         />
       )
